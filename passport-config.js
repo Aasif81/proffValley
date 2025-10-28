6ee47a1f4c3ede2173d20d0cf9ecb0b82f36d8c8
Aasif81 <noah620231@gmail.com>
2025-10-28 12:23:41 +0530

    Update passport-config.js

--- passport-config.js
+++ passport-config.js
@@ -22,7 +22,8 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: process.env.CALLBACK_URL
+
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await User.findOne({ googleId: profile.id });
