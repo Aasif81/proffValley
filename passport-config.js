6bc4fa7bcb0dca940ddf85617e82eec635d2444a
Aasif81 <noah620231@gmail.com>
2025-11-04 12:00:18 +0530

    Update passport-config.js

--- passport-config.js
+++ passport-config.js
@@ -20,29 +20,32 @@ passport.deserializeUser(async (id, done) => {
 // Google
 if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
-
-  }, async (accessToken, refreshToken, profile, done) => {
-    try {
-      let user = await User.findOne({ googleId: profile.id });
-      if (!user) {
-        user = await User.findOne({ email: profile.emails?.[0]?.value });
-      }
-      if (!user) {
-        user = await User.create({
-          username: profile.displayName,
-          email: profile.emails?.[0]?.value || `no-email-${profile.id}@example.com`,
-          googleId: profile.id
-        });
-      } else if (!user.googleId) {
-        user.googleId = profile.id;
-        await user.save();
-      }
-      done(null, user);
-    } catch (err) { done(err); }
-  }));
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.NODE_ENV === "production"
+    ? "https://proffvalley.onrender.com/auth/google/callback"
+    : "http://localhost:3000/auth/google/callback"
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+    let user = await User.findOne({ googleId: profile.id });
+    if (!user) {
+      user = await User.findOne({ email: profile.emails?.[0]?.value });
+    }
+    if (!user) {
+      user = await User.create({
+        username: profile.displayName,
+        email: profile.emails?.[0]?.value || `no-email-${profile.id}@example.com`,
+        googleId: profile.id
+      });
+    } else if (!user.googleId) {
+      user.googleId = profile.id;
+      await user.save();
+    }
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+}));
 }
 
 // Microsoft (Azure AD OAuth2)
